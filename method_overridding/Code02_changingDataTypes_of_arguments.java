f0632a114301722f97d71b9fec64ff2151017f21
Pankaj Addi <p.addi@sap.com>
2023-08-28 16:46:47 +0530

    Git Commit

---
+++ method_overridding/Code02_changingDataTypes_of_arguments.java
@@ -0,0 +1,18 @@
+package method_overridding;
+
+class MethodOverLoadingAdder02 {
+    static int add(int a, int b) {
+        return a + b;
+    }
+
+    static double add(double a, double b) {
+        return a + b;
+    }
+}
+
+class TestOverloading2 {
+    public static void main(String[] args) {
+        System.out.println(MethodOverLoadingAdder02.add(11, 11));
+        System.out.println(MethodOverLoadingAdder02.add(12.3, 12.6));
+    }
+}
