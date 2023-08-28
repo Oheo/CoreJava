6a510646f22e7bece67c2c0fe9e019513f970e0f
Pankaj Addi <p.addi@sap.com>
2023-08-28 16:59:09 +0530

    Git Commit

---
+++ method_overridding/TestOverloading_any_number_of_main_methods4.java
@@ -0,0 +1,18 @@
+package method_overridding;
+
+// You can have any number of main methods in a class by method overloading.
+// But JVM calls main() method which receives string array as arguments only.
+
+class TestOverloading_any_number_of_main_methods4 {
+    public static void main(String[] args) {
+        System.out.println("main with String[]");
+    }
+
+    public static void main(String args) {
+        System.out.println("main with String");
+    }
+
+    public static void main() {
+        System.out.println("main without args");
+    }
+}
